dcffb3c77acfe30bb7ec12acaaed2de4a819e81a
Ramin-Asady <ramin_asady@hotmail.com>
2023-04-12 10:55:41 +0430

    Adding Profile table and make single_project page
    Now we're ready to assign each projects to website users. Creating Profile table helps us to do that. Project and Review tables need to be connected with this new table. Notive Profile table is inside Users app but those two are inside Projects app.After that I've made a new template so that each project is shown indeviduaslly.

--- projects/models.py
+++ projects/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 import uuid
 
+from users.models import Profile
+
 class Project(models.Model):
-    #owner=models.ForeignKey(Profile, on_delete=models.CASCADE,blank=True,null=True)
+    owner=models.ForeignKey(Profile, on_delete=models.CASCADE,blank=True,null=True)
     title=models.CharField(max_length=200)
     description=models.TextField(null=True,blank=True)
     featured_image=models.ImageField(null=True,blank=True, default='default.jpg',upload_to="ProjectImages/")
@@ -22,13 +24,29 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+    
+    @property
+    def getVoteCount(self):
+        reviews=self.review_set.all()
+        up_votes=reviews.filter(value='UP').count()
+        vote_total=reviews.count()
+
+        if vote_total == 0:
+            ratio=0
+        else:
+           ratio= (up_votes/vote_total) * 100
+
+        self.vote_total=vote_total
+        self.vote_ratio=ratio
+
+        self.save()
 
     class Meta:
         ordering=['-vote_ratio' , '-vote_total' , 'title']
 
 class Review(models.Model):
     VOTE_TYPE=( ('UP','UP_VOTE'),( 'DOWN' , 'DOWN_VOTE'))
-    #owner=models.ForeignKey(Profile, on_delete=models.CASCADE,blank=True,null=True)
+    owner=models.ForeignKey(Profile, on_delete=models.CASCADE,blank=True,null=True)
     project= models.ForeignKey(Project, on_delete=models.CASCADE)
     body=models.TextField(null=True,blank=True)
     value=models.CharField(max_length=200,choices=VOTE_TYPE)
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Project
+
+from .forms import ReviewForm
+
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,3 +11,10 @@ def projects(request):
     projects=Project.objects.all()
 
     return render(request,'projects.html' , {"projects":projects})
+
+def single_project(request,pk):
+    project=Project.objects.get(title=pk)
+    tags=project.tags.all()
+
+    context={'project':project,'tags':tags}
+    return render(request,'single_project.html',context)
