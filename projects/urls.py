ece3f17d93c8749282eab9a21029471f4f202a72
Ramin-Asady <ramin_asady@hotmail.com>
2023-04-16 17:31:33 +0430

    Edite and delete reviews functionality
    It would be nice if users can update or even delete their review on a specific project. Once again I've used "Delete.js" in order to delete an object.

--- projects/urls.py
+++ projects/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('create-project/', views.createProject, name="create-project"),
     path('update-project/<str:pk>',views.updateProject,name="update_project"),
     path('delete-project/<str:pk>',views.deleteProject,name="delete_project"),
+
+    path('review/update/<str:pk>', views.reviewUpdate, name="reviewUpdate"),
+    path('review/delete/<str:pk>', views.deleteReview, name="deleteReview"),
 ]
